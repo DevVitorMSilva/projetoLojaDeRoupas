1b523be222e74aedeade8252880c5d070434b29b
Vitor Silva <vitormarcos2005@hotmail.com>
2024-06-19 20:04:34 -0300

    Página sobre nós

--- js/carrinho.js
+++ js/carrinho.js
@@ -2,46 +2,85 @@ let quantidadeItens = 0
 
 var adicionar = document.getElementById('adicionar')
 var remover = document.getElementById('remover')
-var spanQuantidade = document.getElementById('spanQuantidade')
-
+var quantidade = document.getElementById('spanQuantidade')
+var item = document.getElementById('item')
+var precoElement = document.getElementById('preco')
 var tamanhoSelect = document.getElementById('tamanho')
-var tamanho  
 
-adicionar.addEventListener('click', function(event) {
+
+adicionar.addEventListener('click', function (event) {
     event.preventDefault()
     adicionarItem()
 })
 
-function adicionarItem(){
+function adicionarItem() {
     quantidadeItens++
-    spanQuantidade.textContent = quantidadeItens
+    quantidade.textContent = quantidadeItens
     tamanho = tamanhoSelect.value
 }
 
-remover.addEventListener('click', function(event) {
+remover.addEventListener('click', function (event) {
     event.preventDefault()
     removerItem()
-    
+
 })
 
 function removerItem() {
     if (quantidadeItens > 0) {
         quantidadeItens--
-        spanQuantidade.textContent = quantidadeItens
+        quantidade.textContent = quantidadeItens
         tamanho = tamanhoSelect.value
-    }else {
+    } else {
         alert("número inválido")
     }
-    
+
 }
 
 adicionarCarrinho = document.getElementById('adicionarCarrinho')
 
-adicionarCarrinho.addEventListener('click', function(event) {
+adicionarCarrinho.addEventListener('click', function (event) {
     event.preventDefault()
-    window.localStorage.setItem("Tamanho", tamanho)
-    window.localStorage.setItem("Quantidade", quantidadeItens)
+
+    let itemInfo = {
+        item: item.textContent,
+        tamanho: tamanhoSelect.value,
+        quantidade: quantidadeItens,
+        preco: precoElement.textContent
+    }
+    localStorage.setItem("ItemInfo", JSON.stringify(itemInfo))
+
+
 })
+
+window.addEventListener('load', function () {
+    let itemInfo = JSON.parse(this.localStorage.getItem("ItemInfo"))
+    if (itemInfo) {
+        let divItem = this.document.createElement('div')
+        divItem.classList.add('itensCarrinho')
+
+        let nomeItem = document.createElement('p')
+        nomeItem.classList.add('nomeItemCarrinho', 'produtoCarrinho')
+        nomeItem.textContent = itemInfo.item
+
+        let quantidadeItem = document.createElement('p')
+        quantidadeItem.classList.add('quantidadeItemcarrinho', 'produtoCarrinho')
+        quantidadeItem.textContent = itemInfo.quantidade + 'x'
+        
+        let precoItem = document.createElement('p')
+            precoItem.classList.add('precoItemCarrinho', 'produtoCarrinho')
+            precoItem.textContent = itemInfo.preco
+
+        divItem.appendChild(nomeItem)
+        divItem.appendChild(quantidadeItem)
+        divItem.appendChild(precoItem)
+
+        let itensCarrinho = document.querySelector('.itensCarrinho')
+        let ultimoItem = itensCarrinho.lastElementChild
+        itensCarrinho.insertBefore(divItem, ultimoItem.nextSibling)
+    }
+})
+
+
 //ABRIR POP-UP CARRINHO
 const active = document.getElementById("active")
 const inactive = document.getElementById("inactive")
